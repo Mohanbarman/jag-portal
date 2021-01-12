c6ec9cddb081f1ea211bf592ebb596db7fa9a07b
mohanbarman <mohanbarman9711@gmail.com>
2021-01-12 15:52:19 +0530

    Fixing carousel current slide issue

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { useState } from 'react';
+import {useState, useContext} from 'react';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import {
   CarouselProvider,
@@ -8,13 +8,14 @@ import {
   ButtonBack,
   ButtonNext,
   Dot,
+  CarouselContext
 } from 'pure-react-carousel';
-import { IconButton } from "@material-ui/core";
+import {IconButton} from "@material-ui/core";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import { makeStyles } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core';
 import Colors from '../Colors';
-import { carouselContent } from "../Content";
+import {carouselContent} from "../Content";
 
 
 // Button nav button styles
@@ -26,18 +27,7 @@ const useStyles = makeStyles({
 
 
 const Carousel = () => {
-  const classes = useStyles();
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      setScreenWidth(window.innerWidth)
-    })
-  }, [])
-
-
-  return(
+  return (
     <CarouselProvider
       naturalSlideHeight={60}
       naturalSlideWidth={100}
@@ -47,55 +37,67 @@ const Carousel = () => {
       <Slider>
         {/*Carousel Images*/}
         {carouselContent.map((carousel, index) => (
-          <Slide index={index}>
+          <Slide index={index} key={carousel.id}>
             <img className="carousel-image" src={carousel.image} alt=""/>
           </Slide>
         ))}
       </Slider>
 
-      {screenWidth > 600 &&
-        <>
-          {/*Nav buttons*/}
-          <ButtonBack className="carousel-nav-btn carousel-prev-btn">
-            <IconButton classes={classes} color="primary" onClick={() => {
-              setCurrentSlide(p => {
-                if (p > 0)
-                  return p - 1;
-              });
-            }}>
-              <ArrowBackIosIcon/>
-            </IconButton>
-          </ButtonBack>
-          <ButtonNext className="carousel-nav-btn carousel-next-btn">
-            <IconButton classes={classes} color="primary" onClick={() => {
-              setCurrentSlide(p => {
-                if (p < carouselContent.length)
-                  return p + 1
-              })
-            }}>
-              <ArrowForwardIosIcon/>
-            </IconButton>
-          </ButtonNext>
+      <CarouselNavButtons/>
+      <CarouselIndicators/>
 
-          {/*Carousel indicators*/}
-          <div className="carousel-indicator-container">
-            {carouselContent.map((carousel, index) => (
-              <Dot
-                className="carousel-indicator"
-                slide={index}
-                onClick={() => setCurrentSlide(index)}
-                children={
-                  <span
-                    className={"carousel-indicator-count" + (currentSlide === index ? ' carousel-indicator-count-active' : '')}>
-                {carousel.label}
-              </span>
-                }/>
-            ))}
-          </div>
-        </>
-      }
     </CarouselProvider>
   );
 };
+
+const CarouselNavButtons = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <ButtonBack className="carousel-nav-btn carousel-prev-btn">
+        <IconButton classes={classes} color="primary">
+          <ArrowBackIosIcon/>
+        </IconButton>
+      </ButtonBack>
+      <ButtonNext className="carousel-nav-btn carousel-next-btn">
+        <IconButton classes={classes} color="primary">
+          <ArrowForwardIosIcon/>
+        </IconButton>
+      </ButtonNext>
+    </>
+  )
+}
+
+const CarouselIndicators = () => {
+  const carouselContext = useContext(CarouselContext);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    function onChange() {
+      setCurrentSlide(carouselContext.state.currentSlide);
+    }
+
+    carouselContext.subscribe(onChange);
+    return () => carouselContext.unsubscribe(onChange);
+  }, [carouselContext]);
+
+  return (
+    <div className="carousel-indicator-container">
+      {carouselContent.map((carousel, index) => (
+        <Dot
+          className="carousel-indicator"
+          slide={index}
+          key={carousel.id}
+          children={
+            <span
+              className={"carousel-indicator-count" + (currentSlide === index ? ' carousel-indicator-count-active' : '')}>
+                {carousel.label}
+              </span>
+          }/>
+      ))}
+    </div>
+  )
+}
 
 export default Carousel;
