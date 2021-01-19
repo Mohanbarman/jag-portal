db314debacb920f3acdaaf154f4616fdb56e845a
mohanbarman <mohanbarman9711@gmail.com>
2021-01-19 11:18:50 +0530

    Making form grid responsive

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import './index.css';
+import './media.css';
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
