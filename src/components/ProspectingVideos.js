628230895a2fd41ff942f561bfc441b872a01809
mohanbarman <mohanbarman9711@gmail.com>
2021-01-20 15:21:08 +0530

    Using material ui grid instead of native css grid

--- src/components/ProspectingVideos.js
+++ src/components/ProspectingVideos.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import YouTube from "react-youtube";
 import { prospectingVideosContent } from '../Content';
+import {Grid, Paper} from "@material-ui/core";
 
 
 const ProspectingVideos = () => {
-  return(
-    <section className="section">
-      <h1 className="decorated-title-1">Prospecting videos</h1>
-      <div className="prospecting-videos-container">
+  return (
+    <section className='section'>
+     <h1 className="decorated-title-1">Prospecting videos</h1>
+      <Grid container className='prospecting-videos-container' spacing={3}>
         {prospectingVideosContent.map((videoID, index) => (
-          <div className="prospecting-videos-item" key={index}>
-            <YouTube
-              videoId={videoID}
-              containerClassName='youtube-video-item'
-              className='youtube-video-item'
-            />
-          </div>
+          <Grid item xs={12} sm={6} lg={4}>
+              <YouTube
+                videoId={videoID}
+                containerClassName='youtube-video-item'
+                className='youtube-video-item'
+              />
+          </Grid>
         ))}
-      </div>
+      </Grid>
     </section>
   )
 }
