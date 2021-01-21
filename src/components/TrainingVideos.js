0b3a2865340d680d0c8a09904be4fa27888f7e9f
mohanbarman <mohanbarman9711@gmail.com>
2021-01-21 13:55:08 +0530

    Implimenting swiper for trainingvideos

--- src/components/TrainingVideos.js
+++ src/components/TrainingVideos.js
@@ -1,67 +1,43 @@
 import React from 'react';
 import YouTube from "react-youtube";
 import { demoTrainingVideos } from "../Content";
-import { IconButton } from "@material-ui/core";
-import { ArrowBackIos, ArrowForwardIos} from "@material-ui/icons";
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-} from 'pure-react-carousel';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.css';
 
-
-const TrainingVideosCarousel = () => {
-  return(
-    <CarouselProvider
-      naturalSlideHeight={50}
-      naturalSlideWidth={100}
-      totalSlides={demoTrainingVideos.length}
-      visibleSlides={1.1}
-    >
-      <Slider>
-        {demoTrainingVideos.map((videoID, index) => (
-          <Slide index={index} key={index}>
-            <YouTube
-              videoId={videoID}
-              containerClassName='training-video-yt-container'
-              className='training-video-yt-item'
-            />
-          </Slide>
-        ))}
-      </Slider>
-      <TrainingCarouselNavButtons/>
-    </CarouselProvider>
-  )
-}
-
-const TrainingCarouselNavButtons = () => {
-  return (
-    <>
-      <ButtonBack className="carousel-nav-btn carousel-prev-btn carousel-training-videos-nav-btn">
-        <IconButton color="primary">
-          <ArrowBackIos/>
-        </IconButton>
-      </ButtonBack>
-      <ButtonNext className="carousel-nav-btn carousel-next-btn carousel-training-videos-nav-btn">
-        <IconButton color="primary">
-          <ArrowForwardIos/>
-        </IconButton>
-      </ButtonNext>
-    </>
-  )
-}
-
-
+SwiperCore.use([Navigation]);
 
 const TrainingVideos = () => {
+  const breakpoints = {
+    680: {
+      slidesPerView: 2.5,
+    },
+    0: {
+      slidesPerView: 1,
+      height: '40vw'
+    }
+  }
+
   return (
-    <div className='training-videos-content'>
+    <div className='training-videos-container'>
       <h3 className='dashboard-subheading'>Training videos</h3>
-      <div className='training-videos-container-outer'>
-        <TrainingVideosCarousel/>
-      </div>
+      <Swiper
+        breakpoints={breakpoints}
+        spaceBetween={10}
+        slidesPerView={2}
+        onSlideChange={(i) => console.log('slide changed ', i)}
+        navigation
+      >
+        {demoTrainingVideos.map((videoId, index) => (
+          <SwiperSlide key={index}>
+            <YouTube
+              videoId={videoId}
+              containerClassName='training-video-yt-container'
+              opts={{height: '100%', width: '100%'}}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   )
 }
