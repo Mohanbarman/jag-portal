069cf372c8ad1ef486d0ef15e91ca1d96902902b
mohanbarman <mohanbarman9711@gmail.com>
2021-01-23 17:59:06 +0530

    Doing backend integration on edit profile fields (firstName, lastName)

--- src/routes/EditProfile.js
+++ src/routes/EditProfile.js
@@ -1,48 +1,58 @@
-import React, { useContext, useState } from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Navbar from "../components/Navbar";
 import {authContext} from "../context/AuthContext";
 import RoundedImageContainer from "../components/RoundedImageContainer";
 import {Button, TextField} from "@material-ui/core";
 import {authenticatedRoutes} from "../Routes";
-import {utilsContext} from "../context/UtilsContext";
-import {validateEmail} from "../Utils";
+import {SEVERITY, utilsContext} from "../context/UtilsContext";
+import {EDIT_USER} from "../graphql/profileSchemas";
+import {useMutation} from "@apollo/client";
+import PlaceholderProfilePic from "../assets/placeholder-profile.png";
 
 
 const EditProfile = () => {
   const {profile, setProfile} = useContext(authContext);
-  const {setModalState} = useContext(utilsContext);
-  const [firstName, setFirstName] = useState(profile?.firstName)
-  const [lastName, setLastName] = useState(profile?.lastName);
-  const [email, setEmail] = useState(profile?.email);
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [profileImage, setProfileImage] = useState(profile?.profileImage);
+  const {displayModal} = useContext(utilsContext);
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+  // const [email, setEmail] = useState(profile?.email ?? '');
+  // const [currentPassword, setCurrentPassword] = useState('');
+  // const [newPassword, setNewPassword] = useState('');
+  const [editUser] = useMutation(EDIT_USER);
 
-  const _handleSubmit = () => {
-    if (!firstName && !email) {
-      setModalState({
-        isOpen: true,
-        severity: 'error',
-        content: 'Please fill required fields.'
-      })
+  useEffect(() => {
+    if (profile) {
+      setFirstName(profile?.firstName);
+      setLastName(profile?.lastName);
+      setProfileImage(profile?.profilePic);
+    }
+  }, [profile])
+
+  const _handleSubmit = async () => {
+    if (!firstName) {
+      displayModal('Please fill required fields.', SEVERITY.ERROR);
       return 0;
     }
 
-    // Update profile
-    setProfile(p => ({
-      ...p,
-      firstName,
-      lastName,
-      email,
-      profileImage,
-    }))
+    try {
+      const res = await editUser({
+        variables: {
+          firstName: firstName,
+          lastName: lastName,
+          profilePic: profileImage,
+        }
+      });
 
-    // display modal
-    setModalState({
-      severity: 'success',
-      content: 'Profile updated successfully',
-      isOpen: true,
-    })
+      setProfile(res.data?.editUser);
+      localStorage['profile'] = JSON.stringify(res.data?.editUser)
+
+      displayModal('Profile updated successfully', SEVERITY.SUCCESS);
+
+    } catch (e) {
+      displayModal(e.message, SEVERITY.ERROR);
+    }
+
   }
 
   const _handleImageUploadClick = (event) => {
@@ -60,13 +70,13 @@ const EditProfile = () => {
     reader.readAsDataURL(file);
   }
 
-  return(
+  return (
     <>
       <Navbar routes={authenticatedRoutes}/>
       <div className='edit-profile-container'>
         <h3>Edit Profile</h3>
         <div className='edit-profile-image-container'>
-          <RoundedImageContainer image={profileImage}/>
+          <RoundedImageContainer image={profileImage ?? PlaceholderProfilePic}/>
 
           <input
             accept='image/*'
@@ -93,8 +103,8 @@ const EditProfile = () => {
                 value={firstName}
                 onChange={i => setFirstName(i.target.value)}
                 variant='standard'
-                error={firstName.length < 1}
-                helperText={firstName.length < 1 ? 'First name is required' : ''}
+                error={firstName?.length < 1}
+                helperText={firstName?.length < 1 ? 'First name is required' : ''}
               />
               <TextField
                 label='Last name'
@@ -102,14 +112,14 @@ const EditProfile = () => {
                 onChange={i => setLastName(i.target.value)}
                 variant='standard'
               />
-              <TextField
-                label='Email'
-                value={email}
-                onChange={i => setEmail(i.target.value)}
-                variant='standard'
-                error={!validateEmail(email)}
-                helperText={validateEmail(email) ? '' : 'Please enter a valid email'}
-              />
+              {/*<TextField*/}
+              {/*  label='Email'*/}
+              {/*  value={email}*/}
+              {/*  onChange={i => setEmail(i.target.value)}*/}
+              {/*  variant='standard'*/}
+              {/*  error={!validateEmail(email)}*/}
+              {/*  helperText={validateEmail(email) ? '' : 'Please enter a valid email'}*/}
+              {/*/>*/}
             </div>
           </div>
           {/*<div className='edit-profile-change-password-container'>*/}
@@ -130,14 +140,14 @@ const EditProfile = () => {
           {/*      onChange={i => setNewPassword(i.target.value)}*/}
           {/*    />*/}
           {/*  </div>*/}
-            <Button
-              variant='contained'
-              color='primary'
-              onClick={_handleSubmit}
-              className='edit-profile-submit-btn'
-            >
-              Save
-            </Button>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={_handleSubmit}
+            className='edit-profile-submit-btn'
+          >
+            Save
+          </Button>
           {/*</div>*/}
         </div>
       </div>
