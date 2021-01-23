48f5bcec3d2767c1d000213310b8e605f0f1aae3
mohanbarman <mohanbarman9711@gmail.com>
2021-01-23 17:58:25 +0530

    Moving Dashboard to separate route

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,117 +5,23 @@ import CoreLeaders from "../components/CoreLeaders";
 import MissionAndVision from "../components/MissionAndVision";
 import ProspectingVideos from "../components/ProspectingVideos";
 import Footer from "../components/Footer"
-import UpcomingMeetings from "../components/UpcomingMeetings";
-import TrainingVideos from "../components/TrainingVideos";
-import { authContext } from "../context/AuthContext";
-import RoundedImageContainer from '../components/RoundedImageContainer';
-import {IconButton, InputAdornment, Input} from '@material-ui/core';
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import BackgroundLogo from '../assets/watermark-logo.png';
-import CreateNewMeeting from "../components/CreateNewMeeting";
+import {authContext} from "../context/AuthContext";
 import {missionVisionContent} from "../Content";
-import {
-  authenticatedRoutes,
-  unauthenticatedRoutes,
-} from "../Routes";
-import {utilsContext} from "../context/UtilsContext";
+import {authenticatedRoutes, unauthenticatedRoutes,} from "../Routes";
 
-
-// Content for unauthenticated users
-const _unauthenticatedContent = (
-  <>
-    <Navbar routes={unauthenticatedRoutes}/>
-    <Carousel />
-    <CoreLeaders />
-    <MissionAndVision content={missionVisionContent}/>
-    <ProspectingVideos />
-    <Footer />
-  </>
-)
-
-// Content for authenticated users
-const AuthenticatedContent = () => {
-  const { profile } = useContext(authContext);
-  const { setModalState } = useContext(utilsContext);
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      setScreenWidth(window.innerWidth);
-    })
-  }, [])
-
-  const _handleCopy = () => {
-    const landingPageInput = document.getElementById('landing-page-input');
-
-    landingPageInput.select();
-    landingPageInput.setSelectionRange(0, 99999); /* For mobile devices */
-    document.execCommand('copy');
-
-    setModalState({
-      isOpen: true,
-      content: 'Successfully copied to clipboard',
-      severity: 'success',
-    })
-  }
-
-  const landingPageField = (
-    <>
-      <h4 className='dashboard-center-text-right'>Landing page</h4>
-      <Input
-        id='landing-page-input'
-        variant='standard'
-        className='landing-page-url-input'
-        value={profile.landingPageUrl}
-        endAdornment={
-          <InputAdornment position={'end'}>
-            <IconButton onClick={_handleCopy}>
-              <FileCopyIcon/>
-            </IconButton>
-          </InputAdornment>
-        }
-      />
-    </>
-  )
-
-  return (
-    <>
-      <Navbar routes={authenticatedRoutes}/>
-      <div className='home-auth-container-flex'>
-        <section className='home-auth-left-section'>
-          {screenWidth < 900 && (
-            <div className='landing-page-url-mobile'>
-              {landingPageField}
-            </div>
-          )}
-          <UpcomingMeetings />
-          <TrainingVideos />
-        </section>
-        <section className='home-auth-right-section'>
-          <img src={BackgroundLogo} alt='' className='background-logo-dasboard'/>
-          <div className='auth-profile-image-container'>
-            <RoundedImageContainer
-              image={profile.profileImage}
-              label={profile.firstName + ' ' + profile.lastName}
-            />
-          </div>
-          <div className='landing-page-url-container'>
-            {landingPageField}
-          </div>
-          {profile?.isAdmin && (
-            <div className='create-new-meeting-container-right-bar'>
-              <CreateNewMeeting/>
-            </div>
-          )}
-        </section>
-      </div>
-    </>
-  )
-};
 
 const Home = () => {
-  const { isAuthenticated } = useContext(authContext);
-  return (isAuthenticated ? <AuthenticatedContent /> : _unauthenticatedContent)
+  const {isAuthenticated} = useContext(authContext);
+  return (
+    <>
+      <Navbar routes={isAuthenticated ? authenticatedRoutes : unauthenticatedRoutes}/>
+      <Carousel/>
+      <CoreLeaders/>
+      <MissionAndVision content={missionVisionContent}/>
+      <ProspectingVideos/>
+      <Footer/>
+    </>
+  )
 }
 
 export default Home;
