14e25f2393d1079950326b10a74dbac00d3a0910
mohanbarman <mohanbarman9711@gmail.com>
2021-01-23 17:54:39 +0530

    Doing backend integration

--- src/components/UpcomingMeetings.js
+++ src/components/UpcomingMeetings.js
@@ -1,12 +1,13 @@
-import React, {useState, useEffect, useRef, useContext} from 'react';
-import {demoUpcomingMeetings} from "../Content";
+import React, {useContext, useRef} from 'react';
 import {
   Button,
+  CircularProgress,
+  Divider,
+  IconButton,
   List,
   ListItem,
   ListItemSecondaryAction,
   ListItemText,
-  Divider, IconButton,
 } from "@material-ui/core";
 import {ArrowDownwardRounded} from "@material-ui/icons";
 import {utilsContext} from "../context/UtilsContext";
@@ -14,64 +15,45 @@ import {utilsContext} from "../context/UtilsContext";
 
 const UpcomingMeetings = () => {
   const upcomingMeetingsRef = useRef(null);
-  const {upcomingMeetings, setUpcomingMeetings, setModalState} = useContext(utilsContext);
-
-  useEffect(() => {
-    const t = setInterval(() => {
-      const newMeeting = {
-        id: Date.now(),
-        name: `Sem amet massa vel morbi at ${Date.now()}`,
-        date: Date.now(),
-        platform: 'zoom',
-        link: 'https://exmple.com/meeting',
-      }
-
-      // add new meeting to current meetings list
-      setUpcomingMeetings(p => [...p, newMeeting])
-
-      // display dialog
-      setModalState({
-        isOpen: true,
-        severity: 'info',
-        content: 'Got a new meeting',
-      })
-    }, 1000 * 10)
-    return () => clearInterval(t);
-  }, [])
+  const {upcomingMeetings} = useContext(utilsContext);
 
   const _handleScroll = () => {
     upcomingMeetingsRef.current?.scrollBy(0, 100);
   }
 
-  return(
-      <section className='upcoming-meetings-section'>
-        <h3 className='dashboard-subheading'>Upcoming meetings</h3>
-        <List className='upcoming-meetings-list' ref={upcomingMeetingsRef} style={{maxHeight: '40vh', overflow: 'auto'}}>
-
-          {upcomingMeetings.map((meeting) => (
-            <div key={meeting.id}>
-              <ListItem alignItems={'flex-start'} >
-                <ListItemText
-                  primary={meeting.name}
-                  secondary={`${new Date(meeting.date).toLocaleString()} • ${meeting.platform}`}
-                />
-                <ListItemSecondaryAction>
-                  <Button variant='text' color='primary' onClick={() => window.open(meeting.link)}>
-                    Open link
-                  </Button>
-                </ListItemSecondaryAction>
-              </ListItem>
-              <Divider variant="inset" component="li" />
-            </div>
-            )).reverse()}
-
-        </List>
-        <div className='icon-btn-container'>
-          <IconButton onClick={_handleScroll} className='upcoming-meetings-scroll-btn' color='primary' variant='filled'>
-            <ArrowDownwardRounded/>
-          </IconButton>
-        </div>
-      </section>
+  return (
+    <section className='upcoming-meetings-section'>
+      <h3 className='dashboard-subheading'>Upcoming meetings</h3>
+      {upcomingMeetings ? (
+        <>
+          <List className='upcoming-meetings-list' ref={upcomingMeetingsRef}
+                style={{maxHeight: '40vh', overflow: 'auto'}}>
+            {upcomingMeetings?.map((meeting) => (
+              <div key={meeting._id}>
+                <ListItem alignItems={'flex-start'}>
+                  <ListItemText
+                    primary={meeting?.title}
+                    secondary={`${new Date(Number(meeting?.scheduledTime)).toLocaleString()} • ${meeting?.platform}`}
+                  />
+                  <ListItemSecondaryAction>
+                    <Button variant='text' color='primary' onClick={() => window.open(meeting?.link)}>
+                      Open link
+                    </Button>
+                  </ListItemSecondaryAction>
+                </ListItem>
+                <Divider variant="inset" component="li"/>
+              </div>
+            ))}
+          </List>
+          <div className='icon-btn-container'>
+            <IconButton onClick={_handleScroll} className='upcoming-meetings-scroll-btn' color='primary'
+                        variant='filled'>
+              <ArrowDownwardRounded/>
+            </IconButton>
+          </div>
+        </>
+      ) : <CircularProgress className='circular-progress' color='primary'/>}
+    </section>
   )
 }
 
