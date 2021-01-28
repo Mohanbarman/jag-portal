46c2ae91787ac63da051b7808ae69acce085a016
mohanbarman <mohanbarman9711@gmail.com>
2021-01-28 16:29:03 +0530

    Adding load more in upcoming meetings

--- src/components/UpcomingMeetings.js
+++ src/components/UpcomingMeetings.js
@@ -1,24 +1,29 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {
   Button,
   CircularProgress,
-  Divider,
   IconButton,
+  LinearProgress,
   List,
   ListItem,
   ListItemSecondaryAction,
   ListItemText,
 } from "@material-ui/core";
 import {ArrowDownwardRounded} from "@material-ui/icons";
-import {utilsContext} from "../context/UtilsContext";
+import {SEVERITY, utilsContext} from "../context/UtilsContext";
 import {useQuery} from "@apollo/client";
 import {MEETINGS} from "../graphql/meetingSchemas";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 const UpcomingMeetings = () => {
   const upcomingMeetingsRef = useRef(null);
-  const {upcomingMeetings, setUpcomingMeetings} = useContext(utilsContext);
+  const {upcomingMeetings, setUpcomingMeetings, displayModal} = useContext(utilsContext);
   const {data, error, refetch} = useQuery(MEETINGS)
+
+  // for handling pagination
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
 
   const _handleScroll = () => {
     upcomingMeetingsRef.current?.scrollBy(0, 100);
@@ -28,31 +33,56 @@ const UpcomingMeetings = () => {
     if (data && !error) {
       setUpcomingMeetings(data?.meetings?.docs);
     }
-  }, [data, error])
+  }, [])
+
+  const loadMoreMeetings = (event) => {
+    refetch({page: page + 1})
+      .then(r => {
+        setPage(r.data?.meetings?.page);
+        setHasNextPage(r.data?.meetings?.hasNextPage);
+        setUpcomingMeetings(p => [...p, ...r.data?.meetings?.docs]);
+      })
+      .catch(e => displayModal(e.message, SEVERITY.ERROR))
+  }
+
+
+  useEffect(() => {
+    console.log(upcomingMeetings);
+    console.log()
+  }, [upcomingMeetings])
 
   return (
     <section className='upcoming-meetings-section'>
       <h3 className='dashboard-subheading'>Upcoming meetings</h3>
       {upcomingMeetings ? (
         <>
-          <List className='upcoming-meetings-list' ref={upcomingMeetingsRef}
-                style={{maxHeight: '40vh', overflow: 'auto'}}>
-            {upcomingMeetings?.map((meeting) => (
-              <div key={meeting._id}>
-                <ListItem alignItems={'flex-start'}>
-                  <ListItemText
-                    primary={meeting?.title}
-                    secondary={`${new Date(Number(meeting?.scheduledTime)).toLocaleString()} • ${meeting?.platform}`}
-                  />
-                  <ListItemSecondaryAction>
-                    <Button variant='text' color='primary' onClick={() => window.open(meeting?.link)}>
-                      Open link
-                    </Button>
-                  </ListItemSecondaryAction>
+          <List
+            className='upcoming-meetings-list'
+            // onScroll={loadMoreMeetings}
+            ref={upcomingMeetingsRef}
+            style={{maxHeight: '50vh', overflow: 'auto'}}
+          >
+            <InfiniteScroll
+              next={loadMoreMeetings}
+              hasMore={hasNextPage}
+              loader={<LinearProgress color={'primary'}/>}
+              dataLength={upcomingMeetings.length}>
+              {upcomingMeetings?.map((meeting) => (
+                <ListItem>
+                  <div key={meeting._id}>
+                    <ListItemText
+                      primary={meeting?.title}
+                      secondary={`${new Date(Number(meeting?.scheduledTime)).toLocaleString()} • ${meeting?.platform}`}
+                    />
+                    <ListItemSecondaryAction>
+                      <Button variant='text' color='primary' onClick={() => window.open(meeting?.link)}>
+                        Open link
+                      </Button>
+                    </ListItemSecondaryAction>
+                  </div>
                 </ListItem>
-                <Divider variant="inset" component="li"/>
-              </div>
-            ))}
+              ))}
+            </InfiniteScroll>
           </List>
           <div className='icon-btn-container'>
             <IconButton onClick={_handleScroll} className='upcoming-meetings-scroll-btn' color='primary'
@@ -64,6 +94,6 @@ const UpcomingMeetings = () => {
       ) : <CircularProgress className='circular-progress' color='primary'/>}
     </section>
   )
-}
+};
 
 export default UpcomingMeetings;
